c5c0abc8ddc0fcd137f47892ebc33cfbe75f6961
AnneKammenhed <anne.juriado@gmail.com>
2022-06-10 11:37:01 +0000

    add js functions

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,14 +1,53 @@
-//** event listener*/
-document.addEventListener("DOMContentLoaded", function() {
-    let buttons = document.getElementsByTagName("button");
+//**constants */
+const rock_button = document.getElementById("r");
+const scissors_button = document.getElementById("s");
 
-    for (let button of buttons) {
-        button.addEventListener("click", function() {
-            if (this.getAttribute("game-type") === "rock") {
-               alert("You played rock!");
-            } else if (this.getAttribute("game-type") === "scissors"){
-                alert("You played scissors!")
-            }
-        });
+const choiceArea_div = document.getElementsByClassName("choice-area");
+
+const scoreArea_div = document.getElementsByClassName("score-area");
+const playerScore_span = document.getElementsByClassName("player-score");
+const computerScore_span = document.getElementsByClassName("computer-score");
+const playerScore = 0;
+const computerScore = 0;
+
+//**the computer choose function */
+function getComputerChoice() {
+    const choices = ["r", "s"];
+    const randomNumber = Math.floor(Math.random() * 2);
+    return choices[randomNumber];
+}
+
+//**the player choose function */
+function game(userChoice) {   
+    const computerChoice = getComputerChoice();
+    if (userChoice === "r", computerChoice === "s") {
+        console.log("Player wins");
+    } else if (userChoice === "s", computerChoice === "r") {
+        console.log("Computer wins");
+    } else if (userChoice === "s", computerChoice === "s") {
+        console.log("It's a draw");
+    } else if (userChoice === "r", computerChoice === "r") {
+        console.log("It's a draw");
+    } else {
+        console.log("fail");
     }
-});
+} 
+
+//** the game function*/
+function main () {
+
+    // event listener/
+    rock_button.addEventListener("click", function() {
+        game("r");
+    })
+
+    scissors_button.addEventListener("click", function() {
+        game("s");
+    })
+}
+
+main();
+
+//**compare hands */
+
+//**update score */
